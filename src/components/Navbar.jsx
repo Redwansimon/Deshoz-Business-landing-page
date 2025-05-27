c1b7a508144a1a87af3179e5f0e159bc7a2590d6
Redwan Ahmed <redwan.pust@gmail.com>
2025-05-27 21:45:02 +0600

    addin name of the project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,9 +23,10 @@ const Navbar = () => {
         className="fixed top-0 left-0 right-0 bg-white/90 backdrop-blur-sm z-50 border-b border-gray-100 shadow-sm">
             <div className="w-full container mx-auto flex justify-between items-center  px-4 sm:px-6 lg:px-8 md:h-20 h-16">
                 {/* logo */}
-                <div className="flex  items-center gap-1 cursor-pointer">
+                <div className="flex justify-center  items-center gap-1 cursor-pointer">
                     <div className="w-4 h-4 bg-blue-600 rounded-full opacity-75 hover:opacity-100 transition-opacity"></div>
                     <div className="w-4 h-4 -ml-2 bg-red-600 rounded-full opacity-100 hover:opacity-75 transition-opacity"></div>
+                    <h2 className="text-xl font-bold text-gray-600 hover:text-gray-500">DESHOZ</h2>
                 </div>
 
                 {/* mobile menu */}
